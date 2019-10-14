bb3a9b5d8a499a9a1ec862c2f8ab13343e29dcfd
Pedro Alves <pedroalves99@ua.pt>
2019-10-14 18:47:42 +0100

    implementação algoritmo a-star para andar feito em conjunto

--- student.py
+++ student.py
@@ -16,7 +16,7 @@ pygame.init()
 actions_in_queue = queue.Queue(100)
 async def agent_loop(server_address="localhost:8000", agent_name="89221"):
     async with websockets.connect(f"ws://{server_address}/player") as websocket:
-
+       
         # Receive information about static game properties
         await websocket.send(json.dumps({"cmd": "join", "name": agent_name}))
         msg = await websocket.recv()
@@ -29,8 +29,9 @@ async def agent_loop(server_address="localhost:8000", agent_name="89221"):
         SCREEN = pygame.display.set_mode((299, 123))
         SPRITES = pygame.image.load("data/pad.png").convert_alpha()
         SCREEN.blit(SPRITES, (0, 0))
-
+        
         while True:
+           
             try:
                 state = json.loads(
                     await websocket.recv()
@@ -38,21 +39,29 @@ async def agent_loop(server_address="localhost:8000", agent_name="89221"):
                 player_pos = state["bomberman"]
                 wall_list = state["walls"]
                 nearest_wall = entity_finder(player_pos,wall_list) #argumentos(pos do bomberman,lista das pos das paredes no mapa)
-                print(nearest_wall)
                 key = "" 
-                if not near_wall(player_pos,nearest_wall):
-                    if(actions_in_queue.empty):
-                        #senao estiver colado a parede vai ate la
-                        action_moving(player_pos,nearest_wall)
-                else:
-                    actions_in_queue.put("B") #else chamas a funçao das bombas para plantar e fugir
-                
+                if(actions_in_queue.empty()):
+                    if near_wall(player_pos,nearest_wall):
+                        
+                        actions_in_queue.put("B")
+
+                        tres_ultimos = path[-5:]
+                        print(tres_ultimos[::-1])
+                        last_3 = tres_ultimos[::-1]
+                        coord2dir(last_3)
+                        websocket.recv()
+                        for x in range(6):
+                            actions_in_queue.put("")
+                        websocket.recv()
+                    step_pos = side_step(nearest_wall)
+                    websocket.recv()
+                    print(nearest_wall)
+                    path = astar(mapa.map, player_pos, step_pos ,mapa)
+                    websocket.recv()
+                    coord2dir(path)
+                    print(path)
                 key = actions_in_queue.get()
-
-
-                
-                
-                
+                websocket.recv()
                 await websocket.send(
                             json.dumps({"cmd": "key", "key": key})
                         )  # send key command to server - you must implement this send in the AI agent
@@ -64,75 +73,6 @@ def near_wall(bomberman,next_move): # diz se o playes esta colado a uma parede
     if distancia_calculation(bomberman,next_move) == 1:
         return True
     return False
-def action_moving(bomberman, dest_pos):
-
-        
-        bx = bomberman[0]
-        by = bomberman [1]
-        nx= dest_pos[0]
-        ny = dest_pos[1]
-        
-        
-        if ( (even_number(bx) and not even_number(by)) and  (even_number(nx) and not even_number(ny))):  
-            if nx > bx:
-                actions_in_queue.put("d")
-            else:
-                actions_in_queue.put("a")
-            if ny > by :
-                for count in range(ny - by + 1): # mais 1 devido ao range parar uma casa antes
-                    actions_in_queue.put("s")
-            else:
-                for count in range(by-ny+1):
-                    actions_in_queue.put("w")
-            if nx > bx:
-                for count in range(nx-bx+1):
-                    actions_in_queue.put("d")
-            else:
-                for count in range(bx-nx+1):
-                    actions_in_queue.put("a")
-        
-        
-        
-        elif ( (even_number(by) and not even_number(bx)) and  (even_number(ny) and not even_number(nx))):
-            if ny > by:
-                actions_in_queue.put("s")
-            else:
-                actions_in_queue.put("w")
-            if nx > bx :
-                for count in range(nx-bx+1):
-                    actions_in_queue.put("d")
-            else:
-                for count in range(bx-nx+1):
-                    actions_in_queue.put("a")
-            if ny > by:
-                for count in range(ny-by+1):
-                    actions_in_queue.put("s")
-            else:
-                for count in range(by-ny+1):
-                    actions_in_queue.put("w")
-            
-        
-        else :
-            if (even_number(nx) and not even_number(ny)):
-                if ny > by:
-                    actions_in_queue.put("s")  
-                elif ny < by:
-                    actions_in_queue.put("w")
-                elif nx > bx:
-                    actions_in_queue.put("d")
-                else:
-                    actions_in_queue.put("a")
-            else:
-                if nx > bx:
-                    actions_in_queue.put("d")
-                elif nx < bx:
-                    actions_in_queue.put("a")
-                elif ny > by:
-                    actions_in_queue.put("s")
-                else:
-                    actions_in_queue.put("w")
-            
-    
 
 def entity_finder(minha_pos,walls_pos): # funçao para encontrar o objeto mais proximo
     distancia= 1000 # valor alto so para fazer a primeira comparaçao, pode ser alterado no futuro para uma melhor maneira
@@ -142,13 +82,151 @@ def entity_finder(minha_pos,walls_pos): # funçao para encontrar o objeto mais p
             distancia = distancia_tmp
             next_wall = pos
     return next_wall
+
 def distancia_calculation(coord1,coord2):
     return math.sqrt( ((coord1[0] - coord2[0])**2) +  ((coord1[1] - coord2[1])** 2))
 def even_number(number): 
     if (number % 2 == 0):
         return True
-    return False    
+    return False   
 
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __str__(self):
+        return str(self.position)
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, player_pos, dst_pos, mapa):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, player_pos)
+    #print(start_node)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, dst_pos)
+   # print(end_node)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        #print(current_node)
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares adsw
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+            
+            # Make sure walkable terrain
+            if mapa.is_blocked((node_position[0],node_position[1])): #maze[node_position[0]][node_position[1]] != 0:
+                #print("stone")
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            #print(child)
+            open_list.append(child) 
+def coord2dir(lista):
+    anterior = lista[0]
+
+    for elem in lista[1:]:
+        x = elem[0] - anterior[0]
+        y = elem[1] - anterior[1]
+        anterior = elem
+        res = x,y
+        if(res == (0,1)):
+            actions_in_queue.put("s")
+        
+        if(res == (0,-1)):
+            actions_in_queue.put("w")
+
+        if(res == (1,0)):
+            actions_in_queue.put("d")
+
+        if(res == (-1,0)):
+            actions_in_queue.put("a")
+
+def side_step(pos):
+    if even_number(pos[0]):
+        pos = pos[0]-1, pos[1]
+
+    elif even_number(pos[1]):
+        pos = pos[0], pos[1]-1
+
+    else:
+        pos = pos[0], pos[1]-1
+    
+    return pos
 # DO NOT CHANGE THE LINES BELLOW
 # You can change the default values using the command line, example:
 # $ NAME='bombastico' python3 client.py
